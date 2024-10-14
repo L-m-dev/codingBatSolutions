7278f2859fbbd8d0ee1e5cda431e35c887494015
L. M. <81708874+L-m-dev@users.noreply.github.com>
2024-10-14 20:17:08 -0300

    Update README.java

--- README.java
+++ README.java
@@ -117,8 +117,8 @@ public String mixString(String a, String b) {
       result+= b.substring(i, i+1);
     }
   }
-
-
+return result;
+}
 
 //repeat end
 //Given a string and an int n, return a string made of n repetitions of the last n characters of the string. You may assume that n is between 0 and the length of the string, inclusive.
@@ -133,20 +133,16 @@ public String repeatEnd(String str, int n) {
   return result;
 }
 
+//Given a string and an int n, return a string made of the first n characters of the string, followed by the 
+//first n-1 characters of the string, and so on. You may assume that n is between 0 and the length of the string, inclusive (i.e. n >= 0 and n <= str.length()).
 
-
-
-
-
-
-
-
-
-
-
-
-  
+ public String repeatFront(String str, int n) {
+  String prefix = str.substring(0, n);
+  String result = "";
+  //Starting at 1. When starting at 1, careful with middle condition.
+  for(int i=1; i<=n; i++){
+    result += prefix;
+    prefix = str.substring(0, n-i);
+  }
   return result;
 }
-
-
