72ff8452d0794ce549153e3df896be82d60c09f7
L. M. <81708874+L-m-dev@users.noreply.github.com>
2024-10-15 17:07:35 -0300

    Update Array2.java

--- Array2.java
+++ Array2.java
@@ -1,1 +1,32 @@
+public int countEvens(int[] nums) {
+  int evenCounter = 0;
+  for(int i=0; i<nums.length;i++){
+    if(nums[i]%2==0){
+      evenCounter++;
+    }
+  }
+  return evenCounter;
+}
+
+// Array-2 > bigDiff
+public int bigDiff(int[] nums) {
+  int diff = 0;
+  int counter = 0;
+  int smallest = nums[0];
+  int biggest = nums[0];
+  
+  if(nums.length==0){
+    return diff;
+  }
+  while(counter<nums.length){
+    if(nums[counter]>biggest){
+      biggest = nums[counter];
+    }
+    else if(nums[counter]<smallest){
+      smallest = nums[counter];
+    }
+   counter++;
+  }
+  return Math.abs(smallest - biggest);
+}
 
