a5b990f3fa78aef3f0b53583db338f95016449fe
L. M. <81708874+L-m-dev@users.noreply.github.com>
2024-10-15 22:24:01 -0300

    Update Array2.java

--- Array2.java
+++ Array2.java
@@ -155,5 +155,28 @@ public boolean more14(int[] nums) {
   return ones>fours;
 }
 
+public String[] fizzArray2(int n) {
+  String[] res = new String[n];
+  for(int i=0;i<n;i++){
+    res[i]=Integer.toString(i);
+  }
+  return res;
+}
+
+//We'll say that a value is "everywhere" in an array if for every pair of 
+//adjacent elements in the array, at least one of the pair is that value. Return true if the given value is everywhere in the array.
+public boolean isEverywhere(int[] nums, int val) {
+   for(int i=0;i<nums.length-1;i++){
+     //early return
+    if(nums[i] != val && nums[i+1] != val){
+      return false;
+    }
+  }
+  return true;
+}
+
+
+
+
 
 
