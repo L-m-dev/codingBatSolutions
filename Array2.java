cff54e9675454e83acefb8f8a521b874eb45e03f
L. M. <81708874+L-m-dev@users.noreply.github.com>
2024-10-15 23:22:48 -0300

    Update Array2.java

--- Array2.java
+++ Array2.java
@@ -176,6 +176,27 @@ public boolean isEverywhere(int[] nums, int val) {
 }
 
 
+//Given an array of ints, return true if the array contains a 2 next to a 2 or a 4 next to a 4, but not both.
+
+public boolean either24(int[] nums) {
+  int[] numberList = new int[2];
+  numberList[0] = 2;
+  numberList[1] = 4;
+  boolean foundPair=false;
+  for(int i=0;i<nums.length-1;i++){
+    if((nums[i]==numberList[0] && nums[i+1]==numberList[0])||
+        (nums[i]==numberList[1] && nums[i+1]==numberList[1])){
+          if(!foundPair){
+            foundPair = true;
+          } else {
+            return false;
+          }
+        }
+      }
+return foundPair;
+}
+
+
 
 
 
